4b17d8a417889f9949decb935ed98fd41ed8ffee
Yousef Herwees <yousefherwees@gmail.com>
2020-09-01 15:39:27 +0000

    Removing About, Home, and Blog from navbar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,21 +4,9 @@ import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 
 const MenuItems = [
   {
-    path: "/",
-    title: "Home"
-  },
-  {
-    path: "/about",
-    title: "About"
-  },
-  {
-    path: "/blog",
-    title: "Blog"
-  },
-  {
     path: "/contact",
-    title: "Contact"
-  },
+    title: "Contact Us"
+  }
 ]
 
 const ListLink = (props) => (<li><Link to={props.to}>{props.children}</Link></li>)
