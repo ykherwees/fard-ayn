c069e5785c6dff580abb5cd5fa3a6d0761e07606
Yousef Herwees <yousefherwees@gmail.com>
2020-09-01 15:29:15 +0000

    fixing topic link in nav

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -74,7 +74,7 @@ class Navigation extends React.Component {
           return data.allMarkdownRemark.edges
             .filter(edge => !!edge.node.frontmatter.date)
             .map((edge, index) =>
-              <ListLink key={index + MenuItems.length} to={edge.node.slug}>{edge.node.frontmatter.title}</ListLink>
+              <ListLink key={index + MenuItems.length} to={edge.node.frontmatter.slug}>{edge.node.frontmatter.title}</ListLink>
           )
         } 
       }
